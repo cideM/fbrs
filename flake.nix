e164baffadc394e1b21ab1f64be968ef494fc950
Florian Beeres <yuuki@protonmail.com>
2022-02-01 13:33:28 +0100

    Simplify flake
    I no longer need the overlay nor staging which just means compiling
    everything forever

--- flake.nix
+++ flake.nix
@@ -2,21 +2,11 @@
   description = "fbrs.io";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/staging-next";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          (super: self: {
-            zola = self.zola.overrideAttrs
-              (old: {
-                buildInputs = old.buildInputs ++ [ pkgs.libsass ];
-              });
-          })
-        ];
-      };
+      let pkgs = import nixpkgs { inherit system; };
       in
       rec {
         devShell = pkgs.mkShell
