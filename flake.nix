5a5c662746ca0d3f486d7f3c9d9da589bbe01915
Florian Beeres <yuuki@protonmail.com>
2023-05-30 20:10:58 +0200

    Add and use alejandra to format .nix

--- flake.nix
+++ flake.nix
@@ -4,15 +4,20 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in
-      rec {
-        devShell = pkgs.mkShell
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in rec {
+        devShell =
+          pkgs.mkShell
           {
             buildInputs = with pkgs; [
-              nixpkgs-fmt
+              alejandra
               shellcheck
               lua
               pandoc
