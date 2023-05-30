a932520e102355313146a8eeb78e20fbc8d80939
Florian Beeres <yuuki@protonmail.com>
2023-05-30 20:22:59 +0200

    Move resume builder to Nix flake

--- flake.nix
+++ flake.nix
@@ -12,7 +12,25 @@
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {inherit system;};
-      in rec {
+
+        generateResume = pkgs.writeShellApplication {
+          name = "generate-fbrs-resume";
+          runtimeInputs = [pkgs.pandoc];
+          text = ''
+            awk '/+++/ { COUNT += 1; next }; COUNT >= 2 { print $0 }' ./content/resume.md \
+            | pandoc --standalone --embed-resources --standalone --from markdown --to html -o resume.html --metadata title="Florian Beeres" -c ./resume/style.css
+          '';
+        };
+      in {
+        packages = rec {
+          inherit generateResume;
+        };
+        apps = rec {
+          generate-resume = flake-utils.lib.mkApp {
+            drv =
+              self.packages.${system}.generateResume;
+          };
+        };
         devShell =
           pkgs.mkShell
           {
